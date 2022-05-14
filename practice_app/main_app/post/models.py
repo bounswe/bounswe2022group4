632b67d77ae390e2e2042141d5279554f063521d
sanberks <sanberkserbest@gmail.com>
2022-05-14 22:09:08 +0300

    database is added to the app

--- practice_app/main_app/post/models.py
+++ practice_app/main_app/post/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date = models.DateTimeField(default=timezone.now)   # the date and time when the post is created.
+    author = models.ForeignKey(User, on_delete=models.CASCADE) # if the user is deleted,their post will also be deleted.
+
+    def __str__(self):
+        return self.title
+
--- practice_app/main_app/post/views.py
+++ practice_app/main_app/post/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
-posts = [
-    {
-        'title' : 'Post 1',
-        'author' : 'Sanberk',
-        'body' : 'This is the first post.',
-        'date' : '14/05/2022'
-    },
-    {
-        'title': 'Post 2',
-        'author': 'Sanberk',
-        'body' : 'This is the second post.',
-        'date': '14/05/2022'
-    }
-]
-
 def home(request):
     context = {
-        'posts' : posts
+        'posts' : Post.objects.all()
     }
     return render(request, 'post/home.html', context)
