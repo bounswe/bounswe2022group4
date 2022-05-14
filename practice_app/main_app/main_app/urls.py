86e1b1452404c75e36807042701bedfb06ca2065
BeratDamar <berat_damar7@hotmail.com>
2022-05-15 01:32:42 +0300

    Adding registration page for users

--- practice_app/main_app/main_app/urls.py
+++ practice_app/main_app/main_app/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', user_views.register_user, name='register'),
     path('', include('post.urls')),
 ]
