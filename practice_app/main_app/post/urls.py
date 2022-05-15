d8167933dc0434efa8d776eb35ed0c261f995082
sanberks <sanberkserbest@gmail.com>
2022-05-15 18:40:51 +0300

    create-post is implemented and location field is added to the posts.

--- practice_app/main_app/post/urls.py
+++ practice_app/main_app/post/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from . import views
+from .views import PostCreateView
 
 urlpatterns = [
     path('', views.home, name='home-page'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
 ]
 
