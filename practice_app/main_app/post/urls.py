8531c33439dd306d72dddcbe0b0165c0ddfdd1c7
sanberks <sanberkserbest@gmail.com>
2022-05-16 03:57:19 +0300

    page for posts of a specific user is added. Also, the list of posts are split into pages.

--- practice_app/main_app/post/urls.py
+++ practice_app/main_app/post/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from . import views
-from .views import PostCreateView, PostDeleteView, PostDetailView, PostUpdateView
+from .views import PostCreateView, PostDeleteView, PostDetailView, PostUpdateView, UserPostListView, PostListView
 
 urlpatterns = [
-    path('', views.home, name='home-page'),
+    path('', PostListView.as_view(), name='home-page'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
 ]
 
