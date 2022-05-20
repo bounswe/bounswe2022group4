9df3f2cb3f2eb9d8411b0b48c193991e72bfc2fe
yigit-can-ozkaya <yigitcanozkaya@hotmail.com>
2022-05-20 03:04:58 +0300

    Dockerization files implemented

--- practice_app/main_app/main_app/settings.py
+++ practice_app/main_app/main_app/settings.py
@@ -87,7 +87,8 @@ DATABASES = {
         'NAME': os.environ.get('MYSQL_NAME'),
         'USER': os.environ.get('MYSQL_USER'),
         'PASSWORD': os.environ.get('MYSQL_PASSWORD'),
-        'HOST': 'localhost',
+        'HOST': 'db',
+        'PORT': '3306',
         'OPTIONS': {
         'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
         'charset': 'utf8mb4',
