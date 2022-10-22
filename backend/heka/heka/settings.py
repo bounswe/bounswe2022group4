de5543bac62f828090f0d90b405c14e82c2f62cd
MercerFrey <imelih247@gmail.com>
2022-10-22 15:05:41 +0300

    add database to settings

--- backend/heka/heka/settings.py
+++ backend/heka/heka/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'heka.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'heka_db',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
