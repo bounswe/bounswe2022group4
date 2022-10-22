c7681fe3b03cb7e87779c4975928d6dcb84d28ae
MercerFrey <imelih247@gmail.com>
2022-10-22 15:41:43 +0300

    hide django secret

--- backend/heka/heka/settings.py
+++ backend/heka/heka/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ix(!2!huir=f_)=^-!mm=zi#lm8+jimweoyexr0gx7!wl7h677'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
