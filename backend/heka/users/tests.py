f67920fb26b30ecb720eea845c8f27eb8f26c14b
bayndrysf <yusufbyndr@hotmail.com>
2022-10-30 00:17:21 +0300

    Unit tests for register and login implemented.

--- backend/heka/users/tests.py
+++ backend/heka/users/tests.py
@@ -1,3 +1,42 @@
-from django.test import TestCase
+from httplib2 import Response
+from rest_framework.test import APITestCase, APIRequestFactory
+from django.urls import reverse
+from rest_framework import status
+from .views import RegisterView, LoginView, LogoutView, HomeView
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class RegisterTestCase(APITestCase):
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.view = RegisterView.as_view()
+        self.url = reverse('register')
+    def test_register(self):
+        data = {
+            "name":"Canan Karatay",
+            "email":"canan.karatay@gmail.com",
+            "password":"karatay.1359"
+        }
+        request = self.factory.post(self.url, data)
+        response = self.view(request)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+class LoginTestCase(APITestCase):
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.view = LoginView.as_view()
+        self.url = reverse('login')
+        self.user = {
+            "name":"Canan Karatay",
+            "email":"canan.karatay@gmail.com",
+            "password":"karatay.1359"
+        }
+    def test_login(self):
+        request = self.factory.post( reverse('register'), self.user)
+        response = RegisterView.as_view()(request)
+        data = {}
+        data['email'] = self.user['email']
+        data['password'] = self.user['password']
+        request = self.factory.post(self.url, data)
+        response = self.view(request) 
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
--- backend/heka/users/views.py
+++ backend/heka/users/views.py
@@ -12,6 +12,7 @@ import jwt, datetime
 from drf_yasg.utils import swagger_auto_schema 
 from drf_yasg import openapi
 from rest_framework.decorators import api_view
+from rest_framework import status
 
 #@swagger_auto_schema(methods=['post',],request_body=UserSerializer )
 class RegisterView(APIView):
@@ -20,7 +21,7 @@ class RegisterView(APIView):
         serializer = UserSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save() 
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 class LoginView(APIView):
     login_schema = openapi.Schema(
@@ -52,7 +53,7 @@ class LoginView(APIView):
 
         encoded_jwt = jwt.encode(payload, "secret", algorithm="HS256")
         
-        response = Response()
+        response = Response(status=status.HTTP_200_OK)
         response.set_cookie(key="jwt", value=encoded_jwt, httponly=True)
         response.data = {
             "jwt": encoded_jwt
@@ -72,7 +73,7 @@ class HomeView(APIView):
         try:
             decoded_jwt = jwt.decode(token, "secret", algorithms=["HS256"])
         except jwt.ExpiredSignatureError:
-            raise AuthenticationFailed("jwt signature error!")
+            raise AuthenticationFailed("jwt-error!")
 
         user = User.objects.filter(id=decoded_jwt["id"]).first()
         serializer = UserSerializer(user)
@@ -84,7 +85,10 @@ class LogoutView(APIView):
     parameters.append(openapi.Parameter('jwt', in_ = 'cookie', type=openapi.TYPE_STRING))
     @swagger_auto_schema(manual_parameters=parameters) 
     def get(self, request):
-        response = Response()
+        response = Response()         
+        token = request.COOKIES.get("jwt")
+        if not token:
+            raise AuthenticationFailed("unauthenticated user!")
         response.delete_cookie("jwt")
         response.data = {
             "message": "logout success"
