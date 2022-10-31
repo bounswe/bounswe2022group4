9462117f50ece2abb7a287627fa8813e84e6b897
Yiğit Can Özkaya <yigitcanozkaya@Yigits-MBP.home>
2022-10-31 19:54:36 +0300

    last

--- heka-front/src/pages/SignUpPage/SignUpPage.js
+++ heka-front/src/pages/SignUpPage/SignUpPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from "react";
-import { NavLink as Navigate, Link } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { BackendApi } from "../../api";
 import { FaUserCircle, FaKey, FaAddressBook } from "react-icons/fa";
 import { AiFillDownCircle, AiOutlineLogin } from "react-icons/ai";
@@ -9,10 +9,7 @@ const SignUpPage = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
-  const [err_message, setErrMessage] = useState();
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [wrong_email_password, setWrong] = useState();
-  const isValidEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
   const validEmail = (e) => {
     var filter =
       /^\s*[\w\-\+_]+(\.[\w\-\+_]+)*\@[\w\-\+_]+\.[\w\-\+_]+(\.[\w\-\+_]+)*\s*$/;
@@ -21,18 +18,26 @@ const SignUpPage = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (username && password && email && validEmail(email)) {
+    if (
+      username.length !== 0 &&
+      password.length !== 0 &&
+      email &&
+      validEmail(email)
+    ) {
       const response = await BackendApi.postRegister(email, username, password);
-      console.log(response);
       if (response.status === 200) {
         setIsAuthenticated(true);
       } else if (response.status === 400) {
-        setWrong(true);
         alert("This e-mail had already been registered!");
+        setUsername("");
+        setEmail("");
+        setPassword("");
       }
     } else {
-      alert("Please enter a valid e-mail!");
-      setErrMessage(true);
+      alert("Please enter valid registration information!");
+      setUsername("");
+      setEmail("");
+      setPassword("");
     }
   };
 
@@ -46,9 +51,9 @@ const SignUpPage = () => {
             <div className="con">
               <div className="head-form">
                 <h2>Sign Up</h2>
-                <p>
+                <span>
                   <SignUpHead />
-                </p>
+                </span>
               </div>
               <div className="field-set">
                 <div className="input-component">
@@ -63,24 +68,9 @@ const SignUpPage = () => {
                     value={email}
                     onChange={(e) => {
                       setEmail(e.target.value);
-                      setErrMessage(false);
-                      setWrong(false);
                     }}
                   />
                 </div>
-                {err_message ? (
-                  <div className="error-msg">
-                    <i className="fa fa-times-circle"></i>
-                    Please enter a valid email address
-                  </div>
-                ) : null}
-
-                {wrong_email_password && !err_message ? (
-                  <div className="error-msg">
-                    <i className="fa fa-times-circle"></i>
-                    Invalid email, username or password!
-                  </div>
-                ) : null}
                 <div className="input-component">
                   <span className="input-item">
                     <FaUserCircle />
@@ -93,8 +83,6 @@ const SignUpPage = () => {
                     value={username}
                     onChange={(e) => {
                       setUsername(e.target.value);
-                      setErrMessage(false);
-                      setWrong(false);
                     }}
                   />
                 </div>
@@ -112,11 +100,11 @@ const SignUpPage = () => {
                     value={password}
                     onChange={(e) => {
                       setPassword(e.target.value);
-                      setWrong(false);
                     }}
                   ></input>
                 </div>
                 <button className="login-button" onClick={handleSubmit}>
+                  Sign Up
                   <AiOutlineLogin aria-hidden="true" />
                 </button>
               </div>
