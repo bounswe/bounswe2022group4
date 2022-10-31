9d392fc3954d5fe5d1ed03be004917b5c81dfed0
MercerFrey <imelih247@gmail.com>
2022-10-31 19:23:54 +0300

    Increase JWT token expiration time

--- backend/heka/users/views.py
+++ backend/heka/users/views.py
@@ -46,7 +46,7 @@ class LoginView(APIView):
         
         payload = {
             "id": user.id,
-            "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
+            "exp": datetime.datetime.utcnow() + datetime.timedelta(days=1),
             "iat": datetime.datetime.utcnow()
         }
 
