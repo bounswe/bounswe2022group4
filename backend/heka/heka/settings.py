90fdc928d441e75865f3ec40869da6a56856a61f
Yusuf Bayındır <48126002+bayndrysf@users.noreply.github.com>
2022-12-01 17:53:52 +0300

    Update settings.py

--- backend/heka/heka/settings.py
+++ backend/heka/heka/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'corsheaders',
     'users',
     'drf_yasg'
