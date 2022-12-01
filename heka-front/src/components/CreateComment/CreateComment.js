4a973878d1f5bedc1a1e8b1903a0e93e35c83375
umutdenizsenerr <umutdeniz442010@gmail.com>
2022-12-02 00:23:22 +0300

    feat: create comment component implemented

---
+++ heka-front/src/components/CreateComment/CreateComment.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import TextField from '@material-ui/core/TextField';
+import { Button } from '@material-ui/core';
+
+const CreatePost = () => {
+  const [body, setBody] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          id='outlined-multiline-static'
+          label='Comment'
+          multiline
+          rows={4}
+          rowsMax='4'
+          margin='normal'
+          style={{ width: '100%' }}
+          onChange={(e) => setBody(e.target.value)}
+        />
+
+        <br />
+
+        <Button type='submit' variant='outlined'>
+          Comment
+        </Button>
+      </form>
+      <br />
+    </div>
+  );
+};
+
+export default CreatePost;
