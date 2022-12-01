4f1d400366b8ef35cd369a1126024007f8cba70f
umutdenizsenerr <umutdeniz442010@gmail.com>
2022-12-01 21:58:01 +0300

    feat: authentication token structure added

--- heka-front/src/pages/SignUpPage/SignUpPage.js
+++ heka-front/src/pages/SignUpPage/SignUpPage.js
@@ -1,14 +1,14 @@
-import React from "react";
-import { useState } from "react";
-import { Navigate } from "react-router-dom";
-import { BackendApi } from "../../api";
-import { FaUserCircle, FaKey, FaAddressBook } from "react-icons/fa";
-import { AiFillDownCircle, AiOutlineLogin } from "react-icons/ai";
+import React from 'react';
+import { useState } from 'react';
+import { Navigate } from 'react-router-dom';
+import { BackendApi } from '../../api';
+import { FaUserCircle, FaKey, FaAddressBook } from 'react-icons/fa';
+import { AiFillDownCircle, AiOutlineLogin } from 'react-icons/ai';
 
 const SignUpPage = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const validEmail = (e) => {
     var filter =
@@ -25,45 +25,46 @@ const SignUpPage = () => {
       validEmail(email)
     ) {
       const response = await BackendApi.postRegister(email, username, password);
-      if (response.status === 200) {
+      console.log(response);
+      if ((response.status >= 200) & (response.status < 300)) {
         setIsAuthenticated(true);
       } else if (response.status === 400) {
-        alert("This e-mail had already been registered!");
-        setUsername("");
-        setEmail("");
-        setPassword("");
+        alert('This e-mail had already been registered!');
+        setUsername('');
+        setEmail('');
+        setPassword('');
       }
     } else {
-      alert("Please enter valid registration information!");
-      setUsername("");
-      setEmail("");
-      setPassword("");
+      alert('Please enter valid registration information!');
+      setUsername('');
+      setEmail('');
+      setPassword('');
     }
   };
 
   return (
     <>
       {isAuthenticated ? (
-        <Navigate to="/sign-in" replace={true} />
+        <Navigate to='/sign-in' replace={true} />
       ) : (
-        <div className="general-login-container">
-          <form className="general-form-component">
-            <div className="con">
-              <div className="head-form">
+        <div className='general-login-container'>
+          <form className='general-form-component'>
+            <div className='con'>
+              <div className='head-form'>
                 <h2>Sign Up</h2>
                 <span>
                   <SignUpHead />
                 </span>
               </div>
-              <div className="field-set">
-                <div className="input-component">
-                  <span className="input-item">
+              <div className='field-set'>
+                <div className='input-component'>
+                  <span className='input-item'>
                     <FaAddressBook />
                   </span>
                   <input
-                    className="form-input"
-                    type="text"
-                    placeholder="Email"
+                    className='form-input'
+                    type='text'
+                    placeholder='Email'
                     required
                     value={email}
                     onChange={(e) => {
@@ -71,14 +72,14 @@ const SignUpPage = () => {
                     }}
                   />
                 </div>
-                <div className="input-component">
-                  <span className="input-item">
+                <div className='input-component'>
+                  <span className='input-item'>
                     <FaUserCircle />
                   </span>
                   <input
-                    className="form-input"
-                    type="text"
-                    placeholder="Username"
+                    className='form-input'
+                    type='text'
+                    placeholder='Username'
                     required
                     value={username}
                     onChange={(e) => {
@@ -86,16 +87,16 @@ const SignUpPage = () => {
                     }}
                   />
                 </div>
-                <div className="input-component">
-                  <span className="input-item">
+                <div className='input-component'>
+                  <span className='input-item'>
                     <FaKey />
                   </span>
 
                   <input
-                    className="form-input"
-                    type="password"
-                    placeholder="Password"
-                    name="password"
+                    className='form-input'
+                    type='password'
+                    placeholder='Password'
+                    name='password'
                     required
                     value={password}
                     onChange={(e) => {
@@ -103,9 +104,9 @@ const SignUpPage = () => {
                     }}
                   ></input>
                 </div>
-                <button className="login-button" onClick={handleSubmit}>
+                <button className='login-button' onClick={handleSubmit}>
                   Sign Up
-                  <AiOutlineLogin aria-hidden="true" />
+                  <AiOutlineLogin aria-hidden='true' />
                 </button>
               </div>
             </div>
@@ -117,11 +118,11 @@ const SignUpPage = () => {
 };
 
 const SignUpHead = () => (
-  <div id="signUpHead">
+  <div id='signUpHead'>
     <label>You can register our application from below</label>
     <br></br>
     <br></br>
-    <AiFillDownCircle aria-hidden="true" />
+    <AiFillDownCircle aria-hidden='true' />
   </div>
 );
 
