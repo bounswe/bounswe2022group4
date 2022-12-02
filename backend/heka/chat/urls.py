1d7192a670a193c3543d155b349e77222cbf0c3d
oguzhantok <tok.oguzhan01@gmail.com>
2022-12-02 09:56:22 -0800

    Updated the urls and settiings files

--- backend/heka/chat/urls.py
+++ backend/heka/chat/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
 from .views import ChatView
 urlpatterns = [
-    # To fetch all messages sent from sender to receiver
-    path('message/<int:receiver>', ChatView.as_view(), name='get-chat'),  # GET request.
-    # To send new message
-    #path('message', views.message_list, name='message-list'),   # POST request
+    path('message/<int:receiver>', ChatView.as_view(), name='get-chat'), 
     path("message", ChatView.as_view(), name='chat'),
 ]
--- backend/heka/heka/settings.py
+++ backend/heka/heka/settings.py
@@ -84,25 +84,14 @@ WSGI_APPLICATION = 'heka.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.environ.get('POSTGRES_NAME'),
-#         'USER': os.environ.get('POSTGRES_USER'),
-#         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-#         'HOST': os.environ.get('POSTGRES_HOST'),
-#         'PORT': 5432,
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'heka_db',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOST'),
+        'PORT': 5432,
     }
 }
 
