b368c2ce4ea61b170cf32f1b52117a9d488f67ea
BeratDamar <berat_damar7@hotmail.com>
2022-12-04 10:22:11 +0300

    Category sidebar is created without backend connection

---
+++ heka-front/src/components/SideBar/SideBar.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import './SideBar.css';
+
+const SideBar = () => {
+
+    return (
+
+        <div className='general-homepage-container'>
+        
+    <div className='home-page-con'>
+    
+        <br></br>
+        <br></br>
+        <h2>Categories</h2>
+        
+        <br></br>
+        
+          
+          <label>Eye Health and Diseases</label>
+          <br></br>
+          <label>Anesthesia and Reanimation</label>
+          <br></br>
+          <label>Cardiology</label>
+          <br></br>
+          <label>Dermatology</label>
+          <br></br>
+          <label>Diagnostic Radiology</label>
+          <br></br>
+          <label>Ear Nose and Throat</label>
+          <br></br>
+          <label>Gastroenterology</label>
+          <br></br>
+
+
+          <label>Urology</label>
+          <br></br>
+          <label>Neurology</label>
+          <br></br>
+          <label>Ophthalmology</label>
+          <br></br>
+          <label>Pathology</label>
+          <br></br>
+          <label>Pediatrics</label>
+          <br></br>
+          <label>Surgery</label>
+          <br></br>
+          <a href="https://www.britannica.com/science/surgery-medicine" rel="noreferrer">
+          Surgery
+         </a>
+         <br></br>
+  
+    </div>
+
+    </div>
+    );
+
+};
+export default SideBar;
