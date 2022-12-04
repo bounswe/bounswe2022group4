f18a5be5873ac71093633f247d7cc26570a361b1
umutdenizsenerr <umutdeniz442010@gmail.com>
2022-12-04 18:29:00 +0300

    post and get comment backend connection

--- heka-front/src/components/CommentBox/CommentBox.js
+++ heka-front/src/components/CommentBox/CommentBox.js
@@ -1,51 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Comment from '../../components/Comment/Comment';
+import { BackendApi } from '../../api';
 
-const CommentBox = ({ isLogged, postId }) => {
-  const [comments, setComments] = useState([
-    {
-      title: 'Headache For Two Days',
-      user: 'umutdenizsenerr',
-      content:
-        'I have ache for two days at left of my head and it is getting worse day by day. \n I dont know what to do. \n I make some research on internet and now i am suspicios about brain tumor please help me.',
-      time: '28.10.2022 14:43',
-    },
-    {
-      title: 'Sprained Ankle',
-      user: 'beratdamar',
-      content:
-        'In the football match I played with my friends 3 days ago, the ball hit my foot very hard and my foot was sprained. It still hurts a lot while walking.',
+const CommentBox = ({
+  isLogged,
+  slug,
+  authenticationToken,
+  changeInComments,
+  setChangeInComments,
+}) => {
+  const [comments, setComments] = useState([]);
 
-      time: '25.10.2022 19:11',
-    },
-    {
-      title: 'Focus Problem',
-      user: 'mbatuhanmalazgirt',
-      content:
-        'Since my exam schedule is very busy these days, I study at the library every day, but I check my phone every five minutes. I am open to any advice.',
-
-      time: '29.10.2022 09.27',
-    },
-    {
-      title: 'Neck Pain',
-      user: 'yigitcanozkaya',
-      content:
-        'Since constantly i work with a computer, my neck have been suffering from looking to it. Doing sports has always been a blessing but it started to not be enough for it',
-
-      time: '30.10.2022 23.27',
-    },
-  ]);
-
+  useEffect(() => {
+    const getComments = async () => {
+      const response = await BackendApi.getComments(slug, authenticationToken);
+      if (response.status >= 200 && response.status < 300) {
+        setComments(response.data);
+        console.log(response.data);
+      }
+    };
+    console.log('xdd');
+    getComments(slug, authenticationToken);
+  }, [changeInComments]);
   return (
     <div style={{ padding: 14 }}>
       {comments.map((comment, index) => (
         <Comment
           index={index}
           key={index}
-          user={isLogged ? comment.user : 'Anonymous'}
-          content={comment.content}
-          time={comment.time}
+          user={isLogged ? comment.username : 'Anonymous'}
+          content={comment.body}
+          time={comment.updated_at}
           isLogged={isLogged}
+          changeInComments={changeInComments}
+          upvote={comment.upvote}
+          downvote={comment.downvote}
+          isExpert={comment.isExpert}
+          setChangeInComments={setChangeInComments}
+          isUpvoted={comment.is_upvoted}
+          isDownvoted={comment.is_downvoted}
         />
       ))}
     </div>
--- heka-front/src/components/CreateComment/CreateComment.js
+++ heka-front/src/components/CreateComment/CreateComment.js
@@ -1,25 +1,42 @@
 import React, { useEffect, useState } from 'react';
 import TextField from '@material-ui/core/TextField';
 import { Button } from '@material-ui/core';
+import { BackendApi } from '../../api';
 
-const CreateComment = () => {
-  const [body, setBody] = useState('');
-
-  const handleSubmit = (event) => {
+const CreateComment = ({
+  slug,
+  authenticationToken,
+  setOpenCreateCommentModal,
+  changeInComments,
+  setChangeInComments,
+}) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
+    const data = {
+      body: event.target.body.value,
+    };
+    const response = await BackendApi.postCreateComment(
+      data.body,
+      slug,
+      authenticationToken
+    );
+
+    console.log(data.body, response);
+    setOpenCreateCommentModal(false);
+    setChangeInComments(!changeInComments);
   };
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <TextField
-          id='outlined-multiline-static'
+          id='body'
           label='Comment'
           multiline
           rows={4}
           rowsMax='4'
           margin='normal'
           style={{ width: '100%' }}
-          onChange={(e) => setBody(e.target.value)}
+          // onChange={(e) => setBody(e.target.value)}
         />
 
         <br />
