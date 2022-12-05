6bf8efbc0b739f98f817b2d8f8ab90644c606357
Yiğit Can Özkaya <yigitcanozkaya@Yigits-MBP.home>
2022-12-05 16:26:55 +0300

    last

--- heka-front/src/api/Backend/Backend.js
+++ heka-front/src/api/Backend/Backend.js
@@ -3,29 +3,26 @@ import ApiInstance from "../ApiInstance";
 export const postLogin = (email, password) => {
   return ApiInstance.post("/api/user/login", { email, password });
 };
-export const postRegister = (email, username, password, isExpert) => {
-  return ApiInstance.post("api/user/register", {
 export const postRegister = (email, username, password, is_expert) => {
-  return ApiInstance.post('api/user/register', {
+  return ApiInstance.post("api/user/register", {
     email,
     username,
     password,
     is_expert,
   });
 };
-export const postCreatePost = (title, body, authenticationToken) => {
-  ApiInstance.setHeader("Authorization", authenticationToken);
-  return ApiInstance.post("api/post/create-post", { title, body });
 
 export const postEmail = (email) => {
-  return ApiInstance.post('/api/user/forget_password', { email});
+  return ApiInstance.post("/api/user/forget_password", { email });
 };
 
-export const postNewPassword = (code,new_password,confirm_new_password) => {
-  return ApiInstance.post('/api/user/reset_password', { code,new_password,confirm_new_password});
+export const postNewPassword = (code, new_password, confirm_new_password) => {
+  return ApiInstance.post("/api/user/reset_password", {
+    code,
+    new_password,
+    confirm_new_password,
+  });
 };
-
-
 
 export const postCreatePost = (
   title,
@@ -34,8 +31,8 @@ export const postCreatePost = (
   image,
   authenticationToken
 ) => {
-  ApiInstance.setHeader('Authorization', authenticationToken);
-  return ApiInstance.post('api/post/create-post', {
+  ApiInstance.setHeader("Authorization", authenticationToken);
+  return ApiInstance.post("api/post/create-post", {
     title,
     body,
     category,
@@ -49,21 +46,6 @@ export const postDeletePost = (slug, authenticationToken) => {
 export const getPosts = (authenticationToken) => {
   ApiInstance.setHeader("Authorization", authenticationToken);
   return ApiInstance.get("api/post/list-posts");
-};
-
-export const sendMessage = (receiver, message, authenticationToken) => {
-  ApiInstance.setHeader("Authorization", authenticationToken);
-  return ApiInstance.post("api/chat/send/message", { receiver, message });
-};
-
-export const fetchMessage = (receiver, authenticationToken) => {
-  ApiInstance.setHeader("Authorization", authenticationToken);
-  return ApiInstance.post("api/chat/fetch/message", { receiver });
-};
-
-export const fetchUsersForChat = (authenticationToken) => {
-  ApiInstance.setHeader("Authorization", authenticationToken);
-  return ApiInstance.get("api/chat/fetch/users");
 };
 export const postCreateComment = (body, slug, authenticationToken) => {
   ApiInstance.setHeader("Authorization", authenticationToken);
@@ -96,34 +78,17 @@ export const postDownvotePost = (slug, authenticationToken) => {
   ApiInstance.setHeader("Authorization", authenticationToken);
   return ApiInstance.post("api/post/downvote-post/" + slug + "/");
 };
-export const postCreateComment = (body, slug, authenticationToken) => {
-  ApiInstance.setHeader('Authorization', authenticationToken);
-  return ApiInstance.post('api/post/create-comment/' + slug + '/', { body });
-};
-export const getComments = (slug, authenticationToken) => {
-  ApiInstance.setHeader('Authorization', authenticationToken);
-  return ApiInstance.get('api/post/fetch-comments/' + slug + '/');
-};
-export const postUpvoteComment = (slug, comment_id, authenticationToken) => {
-  ApiInstance.setHeader('Authorization', authenticationToken);
-  return ApiInstance.post('api/post/upvote-comment/' + slug + '/' + comment_id);
-};
-export const postDownvoteComment = (slug, comment_id, authenticationToken) => {
-  ApiInstance.setHeader('Authorization', authenticationToken);
-  return ApiInstance.post(
-    'api/post/downvote-comment/' + slug + '/' + comment_id
-  );
-};
-export const postDeleteComment = (slug, comment_id, authenticationToken) => {
-  ApiInstance.setHeader('Authorization', authenticationToken);
-  return ApiInstance.post('api/post/delete-comment/' + slug + '/' + comment_id);
+export const sendMessage = (receiver, message, authenticationToken) => {
+  ApiInstance.setHeader("Authorization", authenticationToken);
+  return ApiInstance.post("api/chat/send/message", { receiver, message });
 };
 
-export const postUpvotePost = (slug, authenticationToken) => {
-  ApiInstance.setHeader('Authorization', authenticationToken);
-  return ApiInstance.post('api/post/upvote-post/' + slug + '/');
+export const fetchMessage = (receiver, authenticationToken) => {
+  ApiInstance.setHeader("Authorization", authenticationToken);
+  return ApiInstance.post("api/chat/fetch/message", { receiver });
 };
-export const postDownvotePost = (slug, authenticationToken) => {
-  ApiInstance.setHeader('Authorization', authenticationToken);
-  return ApiInstance.post('api/post/downvote-post/' + slug + '/');
+
+export const fetchUsersForChat = (authenticationToken) => {
+  ApiInstance.setHeader("Authorization", authenticationToken);
+  return ApiInstance.get("api/chat/fetch/users");
 };
