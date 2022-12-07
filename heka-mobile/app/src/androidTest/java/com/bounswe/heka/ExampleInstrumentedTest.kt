b53fcafeb75e5f14ecfe643479e98047a114e848
erdnj <erdnjj@gmail.com>
2022-12-07 03:56:45 +0300

    double validate password unit tests added

--- heka-mobile/app/src/androidTest/java/com/bounswe/heka/ExampleInstrumentedTest.kt
+++ heka-mobile/app/src/androidTest/java/com/bounswe/heka/ExampleInstrumentedTest.kt
@@ -1,12 +1,15 @@
 package com.bounswe.heka
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Before
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +18,13 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.bounswe.heka", appContext.packageName)
     }
+
 }
---
+++ heka-mobile/app/src/test/java/com/bounswe/heka/DoublePasswordValidatorUnitTest.kt
@@ -0,0 +1,44 @@
+package com.bounswe.heka
+
+import com.bounswe.heka.utils.EmailValidator
+import com.bounswe.heka.utils.PasswordValidator
+import junit.framework.Assert
+import org.junit.Test
+
+class DoublePasswordValidatorUnitTest {
+    @Test
+    fun emailValidator_CorrectEmailSimple_ReturnsTrue() {
+        val validPass = listOf<Pair<String,String>>(
+            Pair("validPass123!","validPass123!"),
+            Pair("another23Val!","another23Val!"),
+            Pair("123aA!123","123aA!123"),)
+
+        val unvalidEmails = listOf<Pair<String,String>>(
+            Pair("noSpecialChar1","noSpecialChar1"),
+            Pair("noDecimal!","noDecimal!"),
+            Pair("nouppercase!123","nouppercase!123"),
+            Pair("validBut1!different","validBut!2different"),
+            )
+        
+        val doublepassvalidator = DoublePasswordValidator()
+        for (p in validPass) {
+            Assert.assertTrue(doublepassvalidator.validate(p.first, p.second) == null)
+        }
+        for (p in unvalidEmails) {
+            Assert.assertTrue(doublepassvalidator.validate(p.first, p.second) != null)
+        }
+    }
+}
+
+
+//This func added temporary, in a branch which is waiting for pull request approval have this class in itself. After the merge this gonna be deleted
+class DoublePasswordValidator{
+    val pW: PasswordValidator = PasswordValidator()
+    fun validate(password1: String?, password2: String?): String? {
+        if (password1 == password2) {
+            return pW.validate(password1)
+        }
+        return "Passwords should be equal!"
+    }
+
+}
