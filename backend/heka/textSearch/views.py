f47c7e239fcc30bddd6e40af657779c81576f382
oguzhantok <tok.oguzhan01@gmail.com>
2022-12-24 04:20:11 -0800

    Configured the urls.py and simple view for the Search App.

--- backend/heka/textSearch/views.py
+++ backend/heka/textSearch/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http.response import JsonResponse
+from rest_framework.parsers import JSONParser
+from rest_framework.views import APIView
+from drf_yasg.utils import swagger_auto_schema 
+from users.models import User
+from chat.models import Message                                                   # Our Message model
+from chat.serializers import MessageSerializer # Message Serializer Class
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 
-# Create your views here.
+class SearchView(APIView):
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication]
+
+    @swagger_auto_schema()
+
+    def post(self,request):
+        pass
