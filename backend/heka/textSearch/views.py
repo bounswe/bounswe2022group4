f14e82bb044d64cf01935a3362ea5696cc0e08e7
oguzhantok <tok.oguzhan01@gmail.com>
2022-12-24 12:36:16 -0800

    Updated the query for partial matches and Added the count parameter

--- backend/heka/textSearch/views.py
+++ backend/heka/textSearch/views.py
@@ -1,16 +1,11 @@
-from django.shortcuts import render
-#from django.http.response import JsonResponse,
 from django.http import JsonResponse
-from rest_framework.parsers import JSONParser
 from rest_framework.views import APIView
 from drf_yasg.utils import swagger_auto_schema 
 from users.models import User
 from posts.models import Post
-from chat.models import Message                                                   # Our Message model
-from chat.serializers import MessageSerializer # Message Serializer Class
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchHeadline, SearchRank
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 class SearchUserView(APIView):
     #permission_classes = [IsAuthenticated]
@@ -53,35 +48,39 @@ class SearchPostView(APIView):
     @swagger_auto_schema()
 
     def get(self,request):
-        #query = request.GET.get("query")
+        count = int(request.GET.get("count"))
         query_list = request.GET.get("query").split()
         query = ""
+
         for i in range(len(query_list)):
             query += query_list[i]
+            query += ':*'  # Thanks to this line we can make partial matches 
             if i != len(query_list) - 1:
                 query += " | "
-        print(query)
+        
         response_data = {}
         response_data[f'posts'] = {}
         
         if query:
-            #vector = SearchVector('title', 'body')
             vector = SearchVector('title', weight='A', config='english') + SearchVector('body', weight='B', config='english')
             q = SearchQuery(query, search_type="raw")
-            qs = Post.objects.annotate(rank=SearchRank(vector, q)).filter().order_by("-rank")
-            #qs = Post.objects.filter(title__icontains=query).order_by("id")
-            #rank__gte=0.001
+            qs = Post.objects.annotate(rank=SearchRank(vector, q)).filter(rank__gte=0.001).order_by("-rank")
+
             if qs:
                 response_data[f'posts']["error"] = False
-                response_data[f'posts']["count"] = len(qs)
-                for i in range(len(qs)):
+                count_range = 0
+                if count and (count < len(qs)):
+                    count_range = count
+                else:
+                    count_range = len(qs)
+                response_data[f'posts']["count"] = count_range
+                for i in range(count_range):
                     response_data[f'posts'][f"post_{i+1}"] = {}
                     response_data["posts"][f"post_{i+1}"]['id'] = qs[i].id
                     response_data["posts"][f"post_{i+1}"]['title'] = qs[i].title
                     response_data["posts"][f"post_{i+1}"]['body'] = qs[i].body
-                    response_data["posts"][f"post_{i+1}"]['creator'] = qs[i].creator.username
+                    response_data["posts"][f"post_{i+1}"]['creator_id'] = qs[i].creator.id
                     response_data["posts"][f"post_{i+1}"]['created_at'] = qs[i].created_at
-                    response_data["posts"][f"post_{i+1}"]['rank'] = qs[i].rank
             else:
                 response_data[f'posts']["error"] = True
                 response_data[f'posts']["count"] = 0
