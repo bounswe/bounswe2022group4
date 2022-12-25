f5a6ae74a2226641eac4ec3efe0818ae33ab03f1
MercerFrey <imelih247@gmail.com>
2022-12-25 16:21:11 +0300

    Add GET and POST endpoints for the text annotation

--- backend/heka/annotations/views.py
+++ backend/heka/annotations/views.py
@@ -77,3 +77,57 @@ class TextAnnotationAPIView(APIView):
         annotation = TextAnnotation.objects.get(pk=annotation_id)
         serializer = TextAnnotationSerializer(annotation)
         return Response(serializer.data["json"], status=status.HTTP_200_OK)
+
+class PostTextAnnotationAPIView(APIView):
+    permission_classes = [AllowAny]
+    @swagger_auto_schema(responses={200:TextAnnotationSerializer(many=True)})
+    def get(self, request, post_slug=None):
+        annotation = TextAnnotation.objects.filter(post_slug=post_slug)
+        serializer = TextAnnotationSerializer(annotation, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @swagger_auto_schema(responses={201:TextAnnotationSerializer})
+    def post(self, request, post_slug=None):
+        default_url = request.build_absolute_uri()
+        default_w3c_template = {
+            "@context": "http://www.w3.org/ns/anno.jsonld",
+            "id": "http://example.org/anno20",
+            "type": "Annotation",
+            "body": {
+                "type": "TextualBody",
+                "value": "<p>example!</p>",
+                "format": "text/html",
+                "language": "en"
+
+            },
+            "target": {
+                "source": "http://example.org/post-slug",
+                "selector": {
+                    "type": "TextPositionSelector",
+                    "start": 412,
+                    "end": 795
+                }
+            }
+        }
+
+        try:
+            anno = TextAnnotation(post_slug=post_slug, json=default_w3c_template)
+            anno.save()
+
+            position = request.data["position"]
+
+            default_w3c_template["id"] = default_url.split("post")[0] + str(anno.id)
+            default_w3c_template["body"]["value"] = request.data["data"]["text"]
+            default_w3c_template["target"]["source"] = request.data["data"]["source"]
+
+            default_w3c_template["target"]["selector"]["start"] = position["start"]
+            default_w3c_template["target"]["selector"]["end"] = position["end"]
+
+            anno.json = default_w3c_template
+            serializer = TextAnnotationSerializer(anno)
+            anno.save()
+
+            return Response(serializer.data["json"], status=status.HTTP_201_CREATED)
+        except Exception as e:
+            logging.exception(e)
+            return Response("Wrong request body", status=status.HTTP_400_BAD_REQUEST)
