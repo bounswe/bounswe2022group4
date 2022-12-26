dbf6926eef38f0c4d02b97f7b5dc8d8bbf34d63d
umutdenizsenerr <umutdeniz442010@gmail.com>
2022-12-26 21:59:39 +0300

    bug fix in navbar

--- heka-front/src/components/Navbar/Navbar.js
+++ heka-front/src/components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { NavLink as Link } from 'react-router-dom';
+import { NavLink as Link, useLocation } from 'react-router-dom';
 import './Navbar.css';
 import SearchBar from '../SearchBar/SearchBar';
 import { FaBars } from 'react-icons/fa';
@@ -8,12 +8,13 @@ import logo from './logo.svg';
 const Navbar = () => {
   const [authToken, setAuthToken] = React.useState('');
   const [loggedUser, setLoggedUser] = React.useState('');
+  const location = useLocation();
   useEffect(() => {
     setLoggedUser(localStorage['user']);
-  }, [localStorage['user']]);
+  }, [location]);
   useEffect(() => {
     setAuthToken(localStorage['authToken']);
-  }, [localStorage['authToken']]);
+  }, [location]);
   return (
     <>
       <nav className='navv'>
