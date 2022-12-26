33f34761c2098891a31d22c6147dc1a6c93c6382
Halil Orhan <halilsalihorhan@gmail.com>
2022-12-26 14:19:11 +0300

    init Search Fragment

---
+++ heka-mobile/app/src/main/java/com/bounswe/heka/search/SearchFragment.kt
@@ -0,0 +1,29 @@
+package com.bounswe.heka.search
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.bounswe.heka.databinding.FragmentSearchBinding
+import com.bounswe.heka.home.HomeViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SearchFragment: Fragment() {
+    private lateinit var binding: FragmentSearchBinding
+    private val viewModel: SearchViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSearchBinding.inflate(layoutInflater)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+//        binding.adapter = viewModel.adapter
+        return binding.root
+    }
+
+}
