0124b48f1c6498b6752ee318594d71c4bb1db1e1
Yusuf Bayındır <48126002+bayndrysf@users.noreply.github.com>
2023-01-02 16:44:51 +0300

    Implements unit test for downvoting a comment.

--- backend/heka/posts/tests.py
+++ backend/heka/posts/tests.py
@@ -164,3 +164,11 @@ class CommentTestCase(APITestCase):
         force_authenticate(request, user=self.test_user)
         response = CommentUpvoteAPIView.as_view()(request, **kwargs)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_downvote_comment(self):
+        url_ = '/api/post/downvote-comment/'
+        kwargs = {"slug" : self.test_post.slug, "id": self.comment.id}
+        request = self.factory.post( url_, format="json")
+        force_authenticate(request, user=self.test_user)
+        response = CommentDownvoteAPIView.as_view()(request, **kwargs)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
